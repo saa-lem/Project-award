18330619a11be3c28150590f87feaf9ea3fa560d
Salem Owino <salemowino18@gmail.com>
2020-06-07 00:40:15 +0300

    Another table added

--- wards/models.py
+++ wards/models.py
@@ -15,3 +15,67 @@ class Project(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     project_link = models.URLField(unique=True, blank = True, null =True)
  
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def get_absolute_url(self):
+        return reverse('project-detail',kwargs = {'pk':self.pk} )
+
+    
+    def datepublished(self):
+        return self.created_on.strftime('%B %d %Y')
+
+    
+    @property
+    def design_votes(self):
+       if self.votes.count() == 0:
+           return 5
+       return sum([r.design_votes for r in self.votes.all()]) / self.votes.count()
+
+        
+    @property
+    def content_votes(self):
+       if self.votes.count() == 0:
+           return 5
+       return sum([r.content_votes for r in self.votes.all()]) / self.votes.count()
+
+        
+    @property
+    def usability_votes(self):
+       if self.votes.count() == 0:
+           return 5
+       return sum([r.usability_votes for r in self.votes.all()]) / self.votes.count()
+
+        
+    @property
+    def creativity_votes(self):
+       if self.votes.count() == 0:
+           return 5
+       return sum([r.creativity_votes for r in self.votes.all()]) / self.votes.count()
+
+
+class Review(models.Model):
+    
+    Project = models.ForeignKey(Project, related_name = 'project_reviews',on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+   
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+    def save_reviews(self):
+        self.save()
+
+    def delete_reviews(self):
+        self.delete()
